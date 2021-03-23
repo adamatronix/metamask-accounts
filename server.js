7e1476b92ce1b66dbf601e91206ff4b2bd614313
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2021-03-23 17:11:48 +0800

    master Install and use body parser and cors because it is cool

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function (req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
